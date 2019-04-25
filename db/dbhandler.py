ee71e8a91a37ae94b8d3ef35a36024cbb3ecbef5
yywzgh <yywzgh@gmail.com>
2019-04-25 17:10:03 +0800

    update file

--- db/dbhandler.py
+++ db/dbhandler.py
@@ -1,53 +1,135 @@
-import  sqlalchemy
-from sqlalchemy import  create_engine
+import sqlalchemy
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column,Integer,String #区分大小写
+from sqlalchemy import Column, Integer, String  # 区分大小写
 from sqlalchemy.orm import sessionmaker
 
 # 生成orm基类
 base = declarative_base()
 
-def query_brand(session,Cls,title):
-    records = session.query(Cls).filter_by(title=title).all()
+
+# 根据品牌名称查询品牌 不存在则新增
+def save_brand(session, classobj, title):
+    records = session.query(classobj).filter_by(title=title).all()
     if len(records) == 0:
-        add_records(session,brand(title=title,status="1"))
-        print("add band", title)
-
-def add_records(session, objs):
-   if isinstance(objs, list):
-      session.add_all(objs)
-   else:
-      session.add(objs)
-   session.commit()
+        add_records(session, Brand(title=title, status="1"))
+        print("save_brand", title)
 
 
-def add_records(session, objs):
-   if isinstance(objs, list):
-      session.add_all(objs)
-   else:
-      session.add(objs)
-   session.commit()
+# 根据品牌名称查询型号 不存在则新增
+def save_model(session, classobj, title, brand_id, type_id):
+    records = session.query(classobj).filter_by(title=title, erp_sppp_id=brand_id, erp_spfl_id=type_id).all()
+    if len(records) == 0:
+        add_records(session, Model(title=title, erp_sppp_id=brand_id, erp_spfl_id=type_id, status="1"))
+        print("save_model", title)
 
 
-class brand(base):
+# 查询规格 不存在则新增
+def save_specification(session, classobj, title, type_id, parent_title):
+    records = session.query(classobj).filter_by(title=title, erp_spfl_id=type_id).all()
+    if len(records) == 0:
+        records = session.query(classobj).filter_by(title=parent_title, pid=0, erp_spfl_id=type_id).all()
+        if len(records) == 0:
+            add_records(session, Specification(title=parent_title, pid=0, erp_spfl_id=type_id, status="1"))
+            pid = session.query(classobj).filter_by(title=parent_title, erp_spfl_id=type_id).all()[0].id
+            add_records(session, Specification(title=title, pid=pid, erp_spfl_id=type_id, status="1"))
+        else:
+            pid = records[0].id
+            add_records(session, Specification(title=title, pid=pid, erp_spfl_id=type_id, status="1"))
+    print("save_version", records)
+
+
+# 保存商品
+def save_commodity(session, classobj, commodity_id, commodity_name, brand_id, type_id, model_id, spec_ids):
+    add_records(session, Commodity(title=commodity_name, id=commodity_id, erp_sppp_id=brand_id, erp_spfl_id=type_id, erp_spuxx_id=model_id,erp_spgg_ids=spec_ids, status="1"))
+
+
+def query_brand_id(session, classobj, title):
+    record = session.query(classobj).filter_by(title=title).all()[0]
+    return record.id
+
+
+def query_model_id(session, classobj, title, brand_id, type_id):
+    record = session.query(classobj).filter_by(title=title, erp_sppp_id=brand_id, erp_spfl_id=type_id).all()[0]
+    return record.id
+
+
+# 查询规格ID
+def query_specification_id(session, classobj, title, type_id):
+    records = session.query(classobj).filter_by(title=title, erp_spfl_id=type_id).all()
+    if len(records) == 0:
+        return ""
+    else:
+        record = records[0]
+        print("##############",record.pid, record.pid)
+        return str(record.pid) + "_" + str(record.id) + ","
+
+
+def add_records(session, classobj):
+    if isinstance(classobj, list):
+        session.add_all(classobj)
+    else:
+        session.add(classobj)
+    session.commit()
+
+
+def add_records(session, classobj):
+    if isinstance(classobj, list):
+        session.add_all(classobj)
+    else:
+        session.add(classobj)
+    session.commit()
+
+
+class Brand(base):
     __tablename__ = 'erp_sppp'  # 表名
     id = Column(Integer, primary_key=True)
     title = Column(String(20))
     status = Column(String(20))
 
-#创建连接
+
+class Model(base):
+    __tablename__ = 'erp_spuxx'  # 表名
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100))
+    erp_sppp_id = Column(Integer)
+    erp_spfl_id = Column(Integer)
+    status = Column(String(20))
+
+
+# 规格
+class Specification(base):
+    __tablename__ = 'erp_spgg'  # 表名
+    id = Column(Integer, primary_key=True)
+    pid = Column(Integer)
+    title = Column(String(100))
+    erp_spfl_id = Column(Integer)
+    status = Column(String(20))
+
+
+# 商品
+class Commodity(base):
+    __tablename__ = 'erp_skuxx'  # 表名
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100))
+    erp_sppp_id = Column(Integer)
+    erp_spfl_id = Column(Integer)
+    erp_spuxx_id = Column(Integer)
+    erp_spgg_ids = Column(String(200))
+    status = Column(String(20))
+
+
+# 创建连接
 def create_session():
-    #base.metadata.create_all(engine) #创建表结构
-    engine = create_engine("mysql+pymysql://root:C9LRquJW@11.2.8.0:200/test", encoding='utf-8', echo=True)
-    session_class=sessionmaker(bind=engine) ##创建与数据库的会话，class,不是实例
-    session=session_class()   #生成session实例
+    # base.metadata.create_all(engine) #创建表结构
+
+    session_class = sessionmaker(bind=engine)  ##创建与数据库的会话，class,不是实例
+    session = session_class()  # 生成session实例
     return session
 
-
-#user_obj = brand(title="rr",status="1") #插入你要创建的数据对象，每执行一次都会新增一次数据。
-#session.add(user_obj)  #把要创建的数据对象添加到这个session里
-#session.commit() #提交，使前面修改的数据生效。
-
+# user_obj = brand(title="rr",status="1") #插入你要创建的数据对象，每执行一次都会新增一次数据。
+# session.add(user_obj)  #把要创建的数据对象添加到这个session里
+# session.commit() #提交，使前面修改的数据生效。
 
 
-#query_brand(session,brand, '华为融合产品')
+# query_brand(session,brand, '华为融合产品')
--- excel/excelhandler.py
+++ excel/excelhandler.py
@@ -1,8 +1,11 @@
 import xlrd
 import xlwt
-from datetime import date,datetime
+from datetime import date, datetime
 from db import dbhandler
-from db.dbhandler import brand
+from db.dbhandler import Brand
+from db.dbhandler import Model
+from db.dbhandler import Specification
+from db.dbhandler import Commodity
 
 #文件地址
 file = 'e:\手机0417.xls'
@@ -35,17 +38,103 @@ def read_all_excel():
             cell = sheet1.cell_value(rown, coln)
             print(cell)
 
-def read_brand_excel():
+
+# 1.插入品牌
+def insert_brand_data():
+    wb = xlrd.open_workbook(filename=file)# 打开文件
+    print(wb.sheet_names())# 获取所有表格名字
+    sheet1 = wb.sheet_by_index(0)# 通过索引获取表格
+    print(sheet1.name,sheet1.nrows,sheet1.ncols)
+    #num_rows = sheet1.nrows
+    num_rows = 2
+    num_cols = sheet1.ncols
+    for rown in range(1, num_rows):
+        brand_name = sheet1.cell_value(rown, 2)
+        print(brand_name)
+        dbhandler.save_brand(session, Brand, brand_name)
+
+
+# 2.型号
+def insert_model_data():
     wb = xlrd.open_workbook(filename=file)#打开文件
     print(wb.sheet_names())#获取所有表格名字
     sheet1 = wb.sheet_by_index(0)#通过索引获取表格
-    print(sheet1.name,sheet1.nrows,sheet1.ncols)
+    print(sheet1.name, sheet1.nrows, sheet1.ncols)
     #num_rows = sheet1.nrows
-    num_rows = 1
+    num_rows = 2
     num_cols = sheet1.ncols
-    for rown in range(num_rows):
-        cell = sheet1.cell_value(rown, 2)
-        print(cell)
-        dbhandler.query_brand(session, brand, cell)
+    for rown in range(1, num_rows):
+        type_name = sheet1.cell_value(rown, 0)
+        type_id = sheet1.cell_value(rown, 1)
+        brand_name = sheet1.cell_value(rown, 2)
+        brand_id = dbhandler.query_brand_id(session, Brand, brand_name)
+        print(brand_name)
+        model_name = sheet1.cell_value(rown, 3)
+        dbhandler.save_model(session, Model, model_name, brand_id, type_id)
 
-read_brand_excel()
+
+# 3.插入规格
+def insert_specification_data(num_cols, parent_title):
+    wb = xlrd.open_workbook(filename=file)#打开文件
+    print(wb.sheet_names())#获取所有表格名字
+    sheet1 = wb.sheet_by_index(0)#通过索引获取表格
+    print(sheet1.name, sheet1.nrows, sheet1.ncols)
+    #num_rows = sheet1.nrows
+    num_rows = 2
+    #num_cols = sheet1.ncols
+    for rown in range(1,num_rows):
+        type_id = sheet1.cell_value(rown, 1)
+        title = sheet1.cell_value(rown, num_cols)
+        if title:
+            dbhandler.save_specification(session, Specification, title, type_id, parent_title)
+
+
+# 4.商品
+def insert_commodity_data():
+    wb = xlrd.open_workbook(filename=file)#打开文件
+    print(wb.sheet_names())#获取所有表格名字
+    sheet1 = wb.sheet_by_index(0)#通过索引获取表格
+    print(sheet1.name, sheet1.nrows, sheet1.ncols)
+    #num_rows = sheet1.nrows
+    num_rows = 2
+    num_cols = sheet1.ncols
+    for rown in range(1, num_rows):
+
+        type_name = sheet1.cell_value(rown, 0)
+        type_id = sheet1.cell_value(rown, 1)
+
+        brand_name = sheet1.cell_value(rown, 2)
+        brand_id = dbhandler.query_brand_id(session, Brand, brand_name)
+
+        model_name = sheet1.cell_value(rown, 3)
+        model_id = dbhandler.query_model_id(session, Model, model_name, brand_id, type_id)
+
+        print("型号",model_name,model_id)
+
+        version = sheet1.cell_value(rown, 4)
+        version_id = dbhandler.query_specification_id(session, Specification, version, type_id)
+
+        print("version_id", version_id)
+
+        storage = sheet1.cell_value(rown, 5)
+        storage_id = dbhandler.query_specification_id(session, Specification, storage, type_id)
+        print("storage", storage_id)
+
+        colour = sheet1.cell_value(rown, 6)
+        colour_id = dbhandler.query_specification_id(session, Specification, colour, type_id)
+        print("###### colour", colour_id)
+
+        spec_ids = version_id.join(storage_id).join(colour_id).strip(",")
+
+        print("--------------",spec_ids)
+
+        commodity_name = sheet1.cell_value(rown, 7)
+        commodity_id = sheet1.cell_value(rown, 8)
+
+        dbhandler.save_commodity(session, Commodity, commodity_id, commodity_name, brand_id, type_id, model_id, spec_ids)
+
+
+#insert_specification_data(4,"规格")
+#insert_specification_data(5,"容量")
+#insert_specification_data(6,"颜色")
+insert_commodity_data()
